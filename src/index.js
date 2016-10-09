ea363bcd24723bc945da899bea8756ba573ae2d5
Angelo Paolo Obispo <angelopaolo.obispo@gmail.com>
2016-10-09 17:46:52 +0800

    testing exec instead of spawn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const spawn = require('child_process').spawn;
+// const spawn = require('child_process').spawn;
+const exec = require('child_process').exec;
 const Server = require('./server');
 // const parser = require('./parser/mocha');
 const http = require('http');
@@ -22,29 +23,35 @@ exports.cappu = (options) => {
     process.env['CAPPU_HOSTNAME'] = hostname;
     // process.env['CAPPU_PORT'] = 31213;
 
-    const cmd = spawn(args.join(' '));
-
-    cmd.stdout.on('data', (data) => {
-      // outputLines.push(data);
-      if(data.indexOf('passing') !== -1) {
-        passing = data.match(/(\d passing)/i);
-      } else if(data.indexOf('failing') !== -1) {
-        failing = data.match(/(\d failing)/i);
-      } else if(data.indexOf('pending') !== -1) {
-        pending = data.match(/(\d pending)/i);
+    // const cmd = spawn(args.join(' '));
+    const cmd = exec(args.join(' '), (error, stdout, stderr) => {
+      console.log('stdout: ' + stdout);
+      console.log('stderr: ' + stderr);
+      if (error !== null) {
+        console.log('exec error: ' + error);
       }
     });
+    // cmd.stdout.on('data', (data) => {
+    //   // outputLines.push(data);
+    //   if(data.indexOf('passing') !== -1) {
+    //     passing = data.match(/(\d passing)/i);
+    //   } else if(data.indexOf('failing') !== -1) {
+    //     failing = data.match(/(\d failing)/i);
+    //   } else if(data.indexOf('pending') !== -1) {
+    //     pending = data.match(/(\d pending)/i);
+    //   }
+    // });
 
-    cmd.stderr.on('data', (data) => {
-      // outputLines.push(data);
-      if(data.indexOf('passing') !== -1) {
-        passing = data.match(/(\d passing)/i);
-      } else if(data.indexOf('failing') !== -1) {
-        failing = data.match(/(\d failing)/i);
-      } else if(data.indexOf('pending') !== -1) {
-        pending = data.match(/(\d pending)/i);
-      }
-    });
+    // cmd.stderr.on('data', (data) => {
+    //   // outputLines.push(data);
+    //   if(data.indexOf('passing') !== -1) {
+    //     passing = data.match(/(\d passing)/i);
+    //   } else if(data.indexOf('failing') !== -1) {
+    //     failing = data.match(/(\d failing)/i);
+    //   } else if(data.indexOf('pending') !== -1) {
+    //     pending = data.match(/(\d pending)/i);
+    //   }
+    // });
 
     // cmd.on('close', (code) => {
     //   console.log(`child process exited with code ${code}`);
