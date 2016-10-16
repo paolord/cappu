edcbd2d37fa9052ead87ad23260a01ec072083bf
Angelo Paolo Obispo <angelopaolo.obispo@gmail.com>
2016-10-16 10:37:42 +0800

    modified parser and cli

--- src/cli.js
+++ src/cli.js
@@ -1,12 +1,11 @@
 'use strict';
 
 module.exports = function(argv) {
-
   const cmd = argv[2];
   const args = argv.splice(3, argv.length);
 
   return {
     cmd,
-    args
+    args,
   };
 };
--- src/parser.js
+++ src/parser.js
@@ -20,7 +20,8 @@ function returnIfNotEmpty(list, index, d) {
     if (typeof list[index] !== 'undefined' && list[index] !== null) {
       return list[index];
     }
-  } else if (typeof d !== 'undefined') {
+  }
+  if (typeof d !== 'undefined') {
     return d;
   }
   return '';
@@ -46,10 +47,10 @@ module.exports = (data, cb) => {
       if (data.indexOf('passing') !== -1 ||
           data.indexOf('failing') !== -1 ||
           data.indexOf('pending') !== -1) {
-        passing = data.match(/(\d+ passing)/i)[0] || '';
-        // failing = data.match(/(\d+ failing)/i)[0] || '';
+        passing = returnIfNotEmpty(data.match(/(\d+ passing)/i), 0);
         failing = returnIfNotEmpty(data.match(/(\d+ failing)/i), 0);
-        pending = data.match(/(\d+ pending)/i)[0] || '';
+        pending = returnIfNotEmpty(data.match(/(\d+ pending)/i), 0);
+
         cb([
           passing,
           failing,
@@ -63,29 +64,20 @@ module.exports = (data, cb) => {
       if (data.indexOf('# tests') !== -1 ||
           data.indexOf('# pass') !== -1 ||
           data.indexOf('# fail') !== -1) {
-        console.log(data.match(/(# tests \d+)/i));            
-        console.log(data);
-        // tests = returnIfNotEmpty(data.match(/(\# tests \d+)/i), 0, '0');
-        // pass = returnIfNotEmpty(data.match(/(\# pass \d+)/i), 0, '0');
-        // fail = returnIfNotEmpty(data.match(/(\# fail \d+)/i), 0, '0');
-
-        tests = data.match(/(# tests \d+)/i)[0];
-        pass = data.match(/(# pass \d+)/i)[0];
-        fail = data.match(/(# fail \d+)/i)[0];
-        // cb([
-        //   `Test: ${tests.split(' ')[2]}`,
-        //   `Pass: ${pass.split(' ')[2]}`,
-        //   `Fail: ${fail.split(' ')[2]}`,
-        // ].join('\n'));
-        console.log(tests);
-        console.log(pass);
-        console.log(fail);
+        tests = returnIfNotEmpty(data.match(/(\# tests \d+)/i), 0, '# tests 0');
+        pass = returnIfNotEmpty(data.match(/(\# pass \d+)/i), 0, '# pass 0');
+        fail = returnIfNotEmpty(data.match(/(\# fail \d+)/i), 0, '# fail 0');
+        
         cb([
-          `Test: ${tests}`,
-          `Pass: ${pass}`,
-          `Fail: ${fail}`,
-        ].join('\n'));
+          `Test: ${tests.split(' ')[2]}`,
+          `Pass: ${pass.split(' ')[2]}`,
+          `Fail: ${fail.split(' ')[2]}`,
+        ].join(' '));
       }
+    }
+    if (process.env.NODE_ENV === 'test') {
+      code = 0;
+      detected = false;
     }
   }
 };
