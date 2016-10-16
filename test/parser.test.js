e20be19e4022edd1a108ff40d37330d3a885220a
Angelo Paolo Obispo <angelopaolo.obispo@gmail.com>
2016-10-16 10:36:16 +0800

    modified parser.test

--- test/parser.test.js
+++ test/parser.test.js
@@ -6,14 +6,23 @@ const parser = require('../src/parser');
 
 describe('Parser', () => {
   describe('#mocha', () => {
-    it('should detect the correct result from spec reporter', (done) => {
+    it('should detect the correct result from spec reporter', () => {
       // emulate mocha stdout
       const data = 'mocha --reporter spec ./test/*.unit.js\
                     25 passing (11s)\
                     2 pending';
       parser(data, (results) => {
         expect(results).to.eql('25 passing  2 pending');
-        done();
+      });
+    });
+    it('should detect the correct result from tap reporter', () => {
+      // emulate mocha stdout
+      const data = 'mocha --reporter tap ./test/*.unit.js\
+                    # tests 25\
+                    # pass 25\
+                    # fail 0';
+      parser(data, (results) => {
+        expect(results).to.eql('Test: 25 Pass: 25 Fail: 0');
       });
     });
   });
